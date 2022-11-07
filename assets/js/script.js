26c196a1c2470491fcb3794845a28a5d968dc02f
lestece <loiceleste94@gmail.com>
2022-11-07 19:25:09 +0000

    Apply the modal functionalites also to the instructions button

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,26 @@ document.getElementById("start-quiz").addEventListener("click", function () {
 
 // Get the modal
 var learnMoreModal = document.getElementById("learn-more-modal");
+var instructionsModal = document.getElementById("instructions-modal");
 
 // Get the button that opens the modal
 var learnMoreBtn = document.getElementById("learn-more-btn");
+var instructionsBtn = document.getElementById("instructions-btn");
 
 // Get the <span> element that closes the modal
 var closeLearnMore = document.getElementsByClassName("close-learn-more")[0];
+var closeInstructions =
+  document.getElementsByClassName("close-instructions")[0];
+
 
 // When the user clicks on the button, open the modal
 learnMoreBtn.onclick = function () {
   learnMoreModal.style.display = "block";
 };
+
+instructionsBtn.onclick = function () {
+    instructionsModal.style.display = "block";
+  };
 
 
 
@@ -29,11 +38,15 @@ closeLearnMore.onclick = function () {
   learnMoreModal.style.display = "none";
 };
 
-
+closeInstructions.onclick = function () {
+    instructionsModal.style.display = "none";
+  };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == learnMoreModal) {
     learnMoreModal.style.display = "none";
+  } else if (event.target == instructionsModal) {
+    instructionsModal.style.display = "none";
   }
 };
