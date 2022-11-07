eaf4f1ea014423d032a08d6500d3d7ea1ec28638
lestece <loiceleste94@gmail.com>
2022-11-07 19:02:45 +0000

    Create the quiz page main structure and connect the start quiz button to it

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// When 'Start quiz' button is clicked, go to quiz page
+document.getElementById("start-quiz").addEventListener("click", function () {
+    window.location = "../dosha-quiz.html";
+  });
