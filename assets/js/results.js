a79488ba94264ca95673ff175d9b273221356c05
lestece <loiceleste94@gmail.com>
2022-11-09 11:27:41 +0000

    Save the username in session storage instead of local; write if/else statement to check if (username)

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,10 +1,20 @@
 
 let dosha = localStorage.getItem('dosha');
-let userName = localStorage.getItem('userName');
+let userName = sessionStorage.getItem('userName');
+
+let doshaSpan = document.getElementById('dosha-result');
+let userNameSpan = document.getElementById('user-name');
+let resultsHeading = document.getElementById('results-heading');
 
 //DOM access and initial conditions
-document.getElementById('dosha-result').textContent = dosha;
-document.getElementById('user-name').textContent = `${userName}, `;
+doshaSpan.textContent = dosha;
+
+if (userName){
+    userNameSpan.textContent = `${userName}, `;
+} else {
+resultsHeading.innerHTML = `Your Dosha is ${dosha}!`;
+}
+
 
 
 
