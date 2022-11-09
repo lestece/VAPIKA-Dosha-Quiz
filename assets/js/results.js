f7a5d27a0aa6a0a71a08f4a76dec4abde8b05f1b
lestece <loiceleste94@gmail.com>
2022-11-09 11:10:22 +0000

    Save the username inputted by the user and retrieve it in the results page

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,6 +1,10 @@
 
 let dosha = localStorage.getItem('dosha');
+let userName = localStorage.getItem('userName');
 
 //DOM access and initial conditions
 document.getElementById('dosha-result').textContent = dosha;
+document.getElementById('user-name').textContent = `${userName}, `;
+
+
 
