4dd5d8b4f2f8c4f141db7c03e33438caddb0af2e
lestece <loiceleste94@gmail.com>
2022-11-20 13:28:03 +0000

    Fix username condition in results.js

--- assets/js/results.js
+++ assets/js/results.js
@@ -18,7 +18,7 @@ doshaSpan.textContent = dosha;
  * If an username has been submitted with the form, display it along with the dosha results.
  * Otherwise, display the string below with the result only
  */
-if (userName) {
+if (userName.trim().length !== 0) {
     userNameSpan.textContent = `${userName}, `;
 } else {
     resultsHeading.innerHTML = `Your Dosha is <span id="dosha-result">${dosha} </span>!`;
