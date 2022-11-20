03a3c3245acc5553de0dc88a4855580df5e16cea
lestece <loiceleste94@gmail.com>
2022-11-20 13:10:12 +0000

    Last style improvements

--- assets/js/results.js
+++ assets/js/results.js
@@ -18,11 +18,11 @@ doshaSpan.textContent = dosha;
  * If an username has been submitted with the form, display it along with the dosha results.
  * Otherwise, display the string below with the result only
  */
-if (userName.trim().length !== 0) {
+if (userName) {
     userNameSpan.textContent = `${userName}, `;
 } else {
     resultsHeading.innerHTML = `Your Dosha is <span id="dosha-result">${dosha} </span>!`;
-}
+};
 
 /**
  * Google charts implementation:
